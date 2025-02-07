7a20390641e12789bee8dc4060b48bd1b2e6e78d
Suraj Wijethunge <surajw@swivelgroup.com.au>
2025-02-07 14:37:37 +0530

    updated local ruby version and set a bundler compatible with the ruby version

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,10 @@
 set -o errexit
 
 # Install system dependencies
-apt-get update -qq && apt-get install -y zbar-tools
+apt-get update -qq 
+apt-get install -y zbar-tools
 
+gem install bundler:1.17.3
 # Build commands
 bundle install
 bundle exec rake assets:precompile

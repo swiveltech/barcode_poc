cbc083c1705aeda38d0c6987e2bdca869e697fe3
Suraj Wijethunge <surajw@swivelgroup.com.au>
2025-02-07 14:52:14 +0530

    install ruby via rvm

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
-gem install bundler:1.17.3
+# Install and use specific Ruby version
+curl -sSL https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm install 2.6.6
+rvm use 2.6.6
+
+# Install bundler
+gem install bundler -v '1.17.3'
+
 # Build commands
 bundle install
 bundle exec rake assets:precompile

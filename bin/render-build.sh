744ac5950cb4f86fa6ed3277b513fcacaa6e2b24
Suraj Wijethunge <surajw@swivelgroup.com.au>
2025-02-07 15:14:14 +0530

    add some degug info to check ruby version

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+echo "==> Debug: Starting build process"
+echo "==> Debug: Current Ruby version (before install)"
+ruby -v
+
 # Install rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -9,11 +13,20 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(~/.rbenv/bin/rbenv init -)"
 
 # Install Ruby
+echo "==> Debug: Installing Ruby 2.6.6"
 rbenv install 2.6.6
 rbenv global 2.6.6
 
+echo "==> Debug: Ruby version after rbenv install"
+ruby -v
+which ruby
+
 # Install bundler
+echo "==> Debug: Installing bundler"
 gem install bundler -v '1.17.3'
+
+echo "==> Debug: Bundler version"
+bundle -v
 
 # Build commands
 bundle install
